4dcdf0f268152a73a9d05a6d657009d53782da7a
Roberto Esposito <boborbt@gmail.com>
2014-03-04 10:18:21 +0100

    Updated documentation

--- ext/fast_containers/FastContainers.cpp
+++ ext/fast_containers/FastContainers.cpp
@@ -49,6 +49,11 @@ extern "C" {
   }
 }
 
+/*
+ * Create a new priority queue and returns it.
+ * @param queue_kind [Symbol] specifies whether to build a :min or a :max queue.
+ */
+
 VALUE pq_new(VALUE klass, VALUE queue_kind) {
   if( TYPE(queue_kind) != T_SYMBOL ) {
     rb_raise(rb_eTypeError, "queue_kind parameter must be a symbol");
@@ -68,6 +73,12 @@ VALUE pq_new(VALUE klass, VALUE queue_kind) {
   return data;
 }
 
+/*
+ * Push a new key/value pair into the queue and returns self.
+ * @param obj [Object] the object to be stored into the queue
+ * @param priority [Float] the priority of the object to be inserted
+ * @return [FastContainers::PriorityQueue] self
+ */
 VALUE pq_push(VALUE self, VALUE obj, VALUE priority) {
   fc_pq::PQueue queue;
   Data_Get_Struct(self, fc_pq::PQueue, queue);
@@ -76,6 +87,9 @@ VALUE pq_push(VALUE self, VALUE obj, VALUE priority) {
   return self;
 }
 
+/*
+ * @return [Object] the object at the top of the priority queue.
+ */
 VALUE pq_top(VALUE self) {
   fc_pq::PQueue queue;
   Data_Get_Struct(self, fc_pq::PQueue, queue);
@@ -83,6 +97,9 @@ VALUE pq_top(VALUE self) {
   return (VALUE) fc_pq::top(queue);
 }
 
+/*
+ * @return [Float] the priority of the object at the top of the priority queue.
+ */
 VALUE pq_top_key(VALUE self) {
   fc_pq::PQueue queue;
   Data_Get_Struct(self, fc_pq::PQueue, queue);
@@ -91,6 +108,10 @@ VALUE pq_top_key(VALUE self) {
   return DBL2NUM(priority);
 }
 
+/*
+ * Pops the top most element from the priority queue.
+ * @return [FastContainers::PriorityQueue] self
+ */
 VALUE pq_pop(VALUE self) {
   fc_pq::PQueue queue;
   Data_Get_Struct(self, fc_pq::PQueue, queue);
@@ -101,6 +122,10 @@ VALUE pq_pop(VALUE self) {
   fc_pq::pop(queue);
   return self;
 }
+
+/*
+ * @return [TrueClass,FalseClass] true if the queue is empty
+ */
 
 VALUE pq_empty(VALUE self) {
   fc_pq::PQueue queue;
